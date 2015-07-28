5749b9c302d6db536e5ddc248e38d28c1f08147e
Rafael Lin <rlin@tw-rafaellin.synaptics-inc.local>
2015-07-28 21:03:31 +0800

    First commit for single word complete

---
+++ AutoComplete/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace AutoComplete
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ultraFormattedTextEditor1 = new Infragistics.Win.FormattedLinkLabel.UltraFormattedTextEditor();
+            this.ultraFormattedTextEditor2 = new Infragistics.Win.FormattedLinkLabel.UltraFormattedTextEditor();
+            this.SuspendLayout();
+            // 
+            // ultraFormattedTextEditor1
+            // 
+            this.ultraFormattedTextEditor1.Location = new System.Drawing.Point(16, 36);
+            this.ultraFormattedTextEditor1.Name = "ultraFormattedTextEditor1";
+            this.ultraFormattedTextEditor1.Size = new System.Drawing.Size(621, 119);
+            this.ultraFormattedTextEditor1.TabIndex = 1;
+            this.ultraFormattedTextEditor1.Value = "";
+            this.ultraFormattedTextEditor1.TextChanged += new System.EventHandler(this.ultraFormattedTextEditor1_TextChanged);
+            // 
+            // ultraFormattedTextEditor2
+            // 
+            this.ultraFormattedTextEditor2.Location = new System.Drawing.Point(16, 171);
+            this.ultraFormattedTextEditor2.Name = "ultraFormattedTextEditor2";
+            this.ultraFormattedTextEditor2.Size = new System.Drawing.Size(164, 168);
+            this.ultraFormattedTextEditor2.TabIndex = 2;
+            this.ultraFormattedTextEditor2.Value = new Infragistics.Win.FormattedLinkLabel.ParsedFormattedTextValue("ultraFormattedTextEditor2");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 351);
+            this.Controls.Add(this.ultraFormattedTextEditor2);
+            this.Controls.Add(this.ultraFormattedTextEditor1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Infragistics.Win.FormattedLinkLabel.UltraFormattedTextEditor ultraFormattedTextEditor1;
+        private Infragistics.Win.FormattedLinkLabel.UltraFormattedTextEditor ultraFormattedTextEditor2;
+    }
+}
+
---
+++ AutoComplete/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoComplete
+{
+    public partial class Form1 : Form
+    {
+        public string dict_path = Application.StartupPath + @"\wordsEn.txt";
+        public Trie local_trie = new Trie();
+        public Form1()
+        {
+            InitializeComponent();
+            InitDictionaryFile( dict_path );
+        }
+        public void InitDictionaryFile( string filepath)
+        {
+            StreamReader sr = new StreamReader( filepath );
+            do
+            {
+                string str = sr.ReadLine();
+                local_trie.insert(str);
+            } while (sr.EndOfStream == false );
+            sr.Close();
+        }
+
+        private void ultraFormattedTextEditor1_TextChanged(object sender, EventArgs e)
+        {
+            string tmp_str_group  = ultraFormattedTextEditor1.Text;
+            char [] delimiter = new[] {' ', ',', '.', ':'};
+            string[] group = tmp_str_group.Split(delimiter);
+            string tmp_str = group[group.Length - 1];
+            if( tmp_str.Length < 3 ) 
+                return ;
+            local_trie.traversal( tmp_str );
+            string tmp = "";
+            List<string>.Enumerator enu = local_trie.str_list.GetEnumerator();
+            while (enu.MoveNext())
+            {
+                tmp += enu.Current + "\n";
+            }
+
+            foreach (string str in local_trie.str_list )
+            {
+            }
+            ultraFormattedTextEditor2.Value = tmp;
+        }
+    }
+
+    public class TrieNode
+    {
+        public Boolean isWord;
+        public Dictionary<char, TrieNode> children = new Dictionary<char, TrieNode>();
+        public string desc ;
+        public TrieNode()
+        {
+            isWord = false;
+            desc = "" ;
+        }
+    };
+
+    public class Trie 
+    {
+        private TrieNode root;
+        public Trie() 
+        {
+            root = new TrieNode();
+        }
+
+        // Inserts a word into the trie.
+        public void insert(string word) 
+        {
+            if( word.Length <= 0 ) return ;
+            TrieNode node = root ;
+            for( int i = 0 ; i < word.Length ; i ++ )
+            {
+                if (node.children.ContainsKey(word[i]) == false)
+                    node.children[word[i]] = new TrieNode();
+                node = node.children[word[i] ] ;
+            }
+            node.isWord = true ;
+            node.desc = word ;
+        }
+
+        // Returns if the word is in the trie.
+        public Boolean search(string word) {
+            return retrieve( word , true);
+        }
+
+        // Returns if there is any word in the trie
+        // that starts with the given prefix.
+        public Boolean startsWith(string prefix) {
+            return retrieve( prefix, false ) ;
+        }
+        private Boolean retrieve( string word, Boolean isWord )
+        {
+            int size = word.Length;
+            if( size <= 0 ) return false ;
+            TrieNode node = root ;
+            for( int i = 0 ; i < size ; i ++ ){
+                if( node.children.ContainsKey( word[i] ) == false )
+                    return false ;
+                node = node.children[ word[i] ] ;
+            }
+            if (isWord)
+                return node.isWord;
+            else
+                return true;
+        }
+        public List<string> str_list = new List<string>();
+        public int total_string_num = 0 ;
+        public void DFS( TrieNode tmpTrieNode, int index, string prefix )
+        {
+            if( total_string_num > 15 )
+                return ;
+            if( tmpTrieNode.isWord && index >= prefix.Length)
+            {
+                Console.WriteLine( tmpTrieNode.desc );
+                total_string_num ++ ;
+                str_list.Add( tmpTrieNode.desc );
+            }
+            if ( index < prefix.Length && tmpTrieNode.children.ContainsKey(prefix[index ]) )
+            {
+                DFS(tmpTrieNode.children[prefix[index]], index + 1 ,prefix );
+            } 
+            if( index >= prefix.Length ){
+                Console.Write( prefix ) ;
+                foreach( KeyValuePair< char, TrieNode > pair in tmpTrieNode.children )
+                    DFS(  pair.Value, index, prefix ) ;
+                return;
+            }
+        }
+        public void traversal( string prefix )
+        {
+            TrieNode node = root ;
+            total_string_num = 0 ;
+            str_list.Clear();
+            if( prefix == "" ) return ;
+            DFS( node , 0 , prefix);
+
+        }
+
+    };
+}
